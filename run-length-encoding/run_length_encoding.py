e718cc4ca9289e97896d0d0d8a37f7887b19e913
Yusuf Tran <mumblez@gmail.com>
2017-04-29 16:09:23 +0100

    add better solution link

--- run-length-encoding/run_length_encoding.py
+++ run-length-encoding/run_length_encoding.py
@@ -36,3 +36,31 @@ def encode(string):
         if c[0] == len(string)-1:
             result += process(counter, lastChar)
     return result
+
+# a better solution - http://exercism.io/submissions/fb280fc95a4d4cf4a925e2904fe225e6
+# regex to split and filter string and string slicing
+# groupby from itertools looks very useful
+
+# import re
+# from itertools import groupby
+#
+# def decode(string):
+#     sequence = re.split("(\d+[A-Za-z]{1}|[A-Za-z]{1})", string)
+#     sequence = list(filter(None, sequence))
+#     decoded = ""
+#     for code in sequence:
+#         if len(code) == 1:
+#             decoded += code
+#         else:
+#             decoded += int(code[:-1])*code[-1]
+#     return decoded
+#
+# def encode(string):
+#     split = ["".join(j) for k,j in groupby(string)]
+#     encoded_str = ""
+#     for chars in split:
+#         if len(chars) == 1:
+#             encoded_str += chars[0]
+#         else:
+#             encoded_str += "{}{}".format(len(chars),chars[0])
+#     return encoded_str
