1b81ce8119501d9b7fc832b370179501553400b4
Yusuf Tran <mumblez@gmail.com>
2017-05-01 21:37:54 +0100

    remove unnecessary join

--- anagram/anagram.py
+++ anagram/anagram.py
@@ -1,2 +1,2 @@
 def detect_anagrams(word, candidates):
-    return [w for w in candidates if w.lower() != word.lower() and ''.join(sorted(w.lower())) == ''.join(sorted(word.lower()))]
+    return [w for w in candidates if w.lower() != word.lower() and sorted(w.lower()) == sorted(word.lower())]
