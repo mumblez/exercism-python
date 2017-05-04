224d6be8cce272ea0cdb888c18f121ab974d19d2
Yusuf Tran <mumblez@gmail.com>
2017-05-04 22:01:30 +0100

    use set vs check

--- sum-of-multiples/sum_of_multiples.py
+++ sum-of-multiples/sum_of_multiples.py
@@ -1,3 +1,2 @@
 def sum_of_multiples(limit, nums):
-    multiples = [x for x in range(1, limit) for y in nums if x%y == 0]
-    return sum(set(multiples))
+    return sum(set(x for n in nums for x in range(n, limit, n)))
