576d8c7b3602c39b9159edb5d95f5883680ee024
Yusuf Tran <mumblez@gmail.com>
2017-05-04 21:45:58 +0100

    improve atbash slicing logic with step inside range

--- atbash-cipher/atbash_cipher.py
+++ atbash-cipher/atbash_cipher.py
@@ -1,11 +1,10 @@
 import re
-from math import ceil
 from string import ascii_lowercase
 
 def encode(msg):
     msg = re.sub(r'[^\w]', '', msg).lower()
     msg = msg.translate(str.maketrans(ascii_lowercase, ascii_lowercase[::-1]))
-    return ' '.join([msg[5*x-5:5*x] for x in range(1, ceil(len(msg)/5) +1)])
+    return ' '.join([msg[x:x+5] for x in range(0, len(msg), 5)])
 
 def decode(msg):
     return msg.translate(str.maketrans(ascii_lowercase[::-1], ascii_lowercase)).replace(' ', '')
