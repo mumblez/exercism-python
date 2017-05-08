6eafb9f1c5f4a22e366c3902ba0f6727001d2f19
Yusuf Tran <mumblez@gmail.com>
2017-05-08 22:11:02 +0100

    reduce from functools to multiply series of numbers

---
+++ largest-series-product/largest_series_product.py
@@ -0,0 +1,18 @@
+from functools import reduce
+from string import digits
+
+def largest_product(numString, span):
+    if span == 0: return 1
+    if len(numString) < 1: raise ValueError("empty string")
+    if span > len(numString): raise ValueError("span larger then number of digits")
+    if span < 0: raise ValueError("Negative span")
+    if any(x not in digits for x in numString): raise ValueError("Invalid character")
+
+    highestValue = 0
+    for i in range(len(numString)-span+1):
+        currentValue = reduce(lambda x,y: x*y, [int(x) for x in list(numString[i:i+span])])
+        if currentValue > highestValue:
+            highestValue = currentValue
+
+    return highestValue
+
