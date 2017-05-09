8136c6abb42d230b99a09c18b452412c11ee6970
Yusuf Tran <mumblez@gmail.com>
2017-05-09 22:57:31 +0100

    remove unnecessary checks

--- kindergarten-garden/kindergarten_garden.py
+++ kindergarten-garden/kindergarten_garden.py
@@ -2,15 +2,10 @@ plantMap = {x[0]: x for x in 'Grass Clover Radishes Violets'.split()}
 studentList = 'Alice Bob Charlie David Eve Fred Ginny Harriet Ileana Joseph Kincaid Larry'.split()
 class Garden(object):
 
-    def __init__(self, msg, students=[]):
+    def __init__(self, msg, students=studentList):
         self.rows = msg.split('\n')
-        self.students = students
-        if len(self.students) > 0:
-            self.students.sort()
+        self.students = sorted(students)
 
     def plants(self, student):
-        if len(self.students) == 0:
-            i = studentList.index(student) * 2
-        else:
-            i = self.students.index(student) * 2
+        i = self.students.index(student) * 2
         return [plantMap[x[i+y]] for x in self.rows for y in range(2)]
